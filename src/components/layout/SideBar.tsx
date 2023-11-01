24e1c239e3e8da15ed11b2b79e487d9709c0a778
thumbthing <ilscc0902@gmail.com>
2023-11-02 03:30:27 +0900

    feat: header와 sidebar에 link 기능 추가

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
 export default function SideBar() {
   const sideBarMenu = ['my', 'react', 'next.js', 'JavaScript', 'mongoDB'];
 
-  const sideBarList = sideBarMenu.map((item) => <li key={item}>{item}</li>);
+  const sideBarList = sideBarMenu.map((item) => {
+    const linkUrl = `/${item}`;
+    return (
+      <li key={item} className="hover:text-emerald-600">
+        <Link href={linkUrl} replace>
+          {item}
+        </Link>
+      </li>
+    );
+  });
 
   return (
-    <aside>
+    <aside className="flex-initial m-1 pr-2 border-r-4 border-zinc-200">
       <ul>{sideBarList}</ul>
     </aside>
   );
