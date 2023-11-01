0c8c203344bf10423abebd9a0a24fe420410c426
thumbthing <ilscc0902@gmail.com>
2023-11-02 04:00:08 +0900

    feat: my 페이지 생성, Content List 컴포넌트 적용

---
+++ src/app/my/page.tsx
@@ -0,0 +1,9 @@
+import ContentList from '@/components/layout/ContentList';
+
+export default function MyPage() {
+  return (
+    <main className="flex-1">
+      <ContentList />
+    </main>
+  );
+}
