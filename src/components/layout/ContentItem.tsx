d42447ae7245daf0426b0d92bbab8e03f1d2f2d5
thumbthing <ilscc0902@gmail.com>
2023-11-02 03:59:31 +0900

    feat: 블로그 글 보여줄 ContentItem, ContentList 컴포넌트 생성

---
+++ src/components/layout/ContentItem.tsx
@@ -0,0 +1,3 @@
+export default function ContentItem() {
+  return <article>item</article>;
+}
---
+++ src/components/layout/ContentList.tsx
@@ -0,0 +1,10 @@
+import ContentItem from './ContentItem';
+
+export default function ContentList() {
+  return (
+    <section>
+      list
+      <ContentItem />
+    </section>
+  );
+}
